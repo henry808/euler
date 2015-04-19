c1757a75e1c63a963bc4817d4eee032fd9893f78
Henry Grantham <hgrantham@gmail.com>
2015-04-18 18:50:04 -0700

    brute force solution

--- 001/001.py
+++ 001/001.py
@@ -5,5 +5,8 @@ from __future__ import print_function
 
 
 if __name__ == '__main__':
-    
-    for 
+    sum1 = 0
+    for i in range (0, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum1 += i
+    print(sum1)
