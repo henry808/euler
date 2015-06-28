126cde809b85eca1e1f02632aeb150d336f547dc
Henry Grantham <hgrantham@gmail.com>
2015-06-28 16:38:46 -0700

    wrote two helper functions and some of main function

--- 004/eul004.py
+++ 004/eul004.py
@@ -1,2 +1,24 @@
 #! /usr/bin/python
 from __future__ import print_function
+
+
+def ith_digit(n, i=0):
+    """returns ith digit of n"""
+    return (n/10**i) % 10
+
+
+def is_palindrome(n):
+    """ return true if integer is a plindrome"""
+    # calculate length of digits
+    length = 0
+    while n > 0:
+        length += 1
+        n / 10
+    # check if palindrome
+    for i in range(length/2):
+        if i !=  
+
+    return False
+
+def find palindromic():
+    pass
