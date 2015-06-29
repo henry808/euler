bfde876754cab8f00c588ce56e8b52aef27ac4db
Henry Grantham <hgrantham@gmail.com>
2015-06-28 21:46:25 -0700

    write test for is_palindrome which exposed an infinite loop in is_palindrome

--- 004/test_004.py
+++ 004/test_004.py
@@ -6,3 +6,12 @@ def test_ithdigit():
     results = [5, 4, 3, 2, 1]
     for i in range(4):
         assert(ith_digit(n, i) == results[i])
+
+
+def test_ispalindrome():
+    input_false = [10, 102, 1121, 1112]
+    input_true = [1, 11, 101, 111, 1111, 1221]
+    for i in input_false:
+        assert(is_palindrome(i) is False)
+    for i in input_true:
+        assert(is_palindrome(i) is True)
