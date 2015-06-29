d4ed1cff368e135dc9f55298391649eb30128452
Henry Grantham <hgrantham@gmail.com>
2015-06-28 23:51:28 -0700

    rewrite in a more pythonic way using map

--- 006/eul006.py
+++ 006/eul006.py
@@ -2,11 +2,17 @@
 from __future__ import print_function
 
 
+def sqr(x):
+    return x ** 2
+
+
 def sum_squares(n):
-    sum1 = 0
-    for i in range(1, n + 1):
-        sum1 += i**2
-    return sum1
+    return (sum(map(sqr, range(1, n + 1))))
+    # sum1 = 0
+    # for i in range(1, n + 1):
+    #     sum1 += i**2
+    # return sum1
+
 
 if __name__ == '__main__':
     print(sum_squares(10))
