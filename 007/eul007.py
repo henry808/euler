59eb5f7c83606b8ba1f53d4e0028cab58300dca3
Henry Grantham <hgrantham@gmail.com>
2015-06-29 00:48:26 -0700

    find_prime works and problem is solved

--- 007/eul007.py
+++ 007/eul007.py
@@ -10,9 +10,26 @@ def is_prime(n):
     """ return True if n is a prime number"""
     if n == 2 | n == 3:
         return True
-    if n == 4:
+    if n % 2 == 0 | n % 3 == 0:
         return False
     for i in range(2, int(sqrt(n))+1):
         if n % i == 0:
             return False
     return True
+
+
+def find_prime(nth):
+    """find nth place in prime number sequence"""
+    count = 1
+    number = 2
+    while count < nth:
+        number += 1
+        while not(is_prime(number)):
+            number += 1
+        count += 1
+    return number
+
+
+
+if __name__ == "__main__":
+    print(find_prime(10001))
