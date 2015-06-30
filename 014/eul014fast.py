5630a194a47698b423fb8961d30e39a041327930
Henry Grantham <hgrantham@gmail.com>
2015-06-30 15:38:01 -0700

    working on a faster implementation and tests

--- 014/eul014fast.py
+++ 014/eul014fast.py
@@ -3,8 +3,7 @@ from __future__ import print_function
 from time import time
 
 # Project Euler # 14
-# this solution works, but is slow (77 seeconds)
-
+# this solution works, but is slow
 
 
 def generate_sequence(n):
@@ -19,17 +18,30 @@ def generate_sequence(n):
     return list1
 
 
-def longest(b):    
+def generate_sequence_length(n):
+    """generate sequance starting with n"""
+    list1 = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        list1.append(n)
+    print("list ", list1)
+    return len(list1)
+
+
+def longest(b):
     max1 = 1
     longest1 = 1
     for i in range(1, b + 1):
         # print(generate_sequence(i), "len: ", len(generate_sequence(i)))
-        if max1 < len(generate_sequence(i)):
-            max1 = len(generate_sequence(i))
+        if max1 < generate_sequence_length(i):
+            max1 = generate_sequence_length(i)
             longest1 = i
     return longest1
 
 if __name__ == '__main__':
     start_time = time()
-    print(longest(1000000))
+    print(longest(10))
     print("time passed: ", time() - start_time)
