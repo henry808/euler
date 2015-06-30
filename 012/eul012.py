7ed7f3388500f656774ae1e778d2b7d409320014
Henry Grantham <hgrantham@gmail.com>
2015-06-29 23:08:21 -0700

    add factor function

--- 012/eul012.py
+++ 012/eul012.py
@@ -5,9 +5,20 @@ from __future__ import print_function
 
 
 def gen_triangle():
+    """generator generates triangle sequence"""
     counter = 1
     sum1 = 0
     while True:
         sum1 += counter
         counter += 1
         yield sum1
+
+
+def factor(n):
+    """returns a list of factors given n"""
+    factors = []
+    for i in range(1, n/2):
+        if n % i == 0:
+            factors.append(i)
+    factors.append(n)
+    return factors
