ebbbf762730d11d478fbd13f2a7d701d0acb5f32
Henry Grantham <hgrantham@gmail.com>
2015-06-29 23:17:09 -0700

    add test for factor and factor doesn't work

--- 012/test_012.py
+++ 012/test_012.py
@@ -1,4 +1,4 @@
-from eul012 import gen_triangle
+from eul012 import gen_triangle, factor
 
 
 def test_gen_triangle():
@@ -6,3 +6,12 @@ def test_gen_triangle():
     gen = gen_triangle()
     for i in range(len(expected)):
         assert(gen.next() == expected[i])
+
+
+def test_factor():
+    input = [1, 3, 6, 10, 15, 21, 28]
+    expected = [[1, 3], [1, 2, 3, 6], [1, 2, 5, 10],
+                [1, 3, 5, 15], [1, 3, 7, 21],
+                [1, 2, 4, 7, 14, 28]]
+    for ind, val in enumerate(input):
+        assert(factor(val) == expected[ind])
