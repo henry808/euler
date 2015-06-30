0dad9f8b3cc55c42700a47ddf3eaca7dba5d81cb
Henry Grantham <hgrantham@gmail.com>
2015-06-29 21:27:17 -0700

    found an error where if the last number is a prime, it doesn't catch it and fixed that error

--- 010/eul010.py
+++ 010/eul010.py
@@ -6,19 +6,22 @@ from __future__ import print_function
 # using sieve of Eratosthenes to solve this
 
 def sieve_of_eratosthenes(n):
-    array = [True] * n
+    array = [True] * (n + 1)
     p = 2
     count = 2
     prime_list = []
     while True:
         prime_list.append(p)
         # Make all False that are mutiples of prime
-        for i in range(0, n, p):
+        for i in range(p, n, p):
             array[i] = False
         # Find next prime
         while array[count] is False:
             count += 1
             if count == n:
+                # if last # is a prime, need to add it
+                if array[count] is True:
+                    prime_list.append(count)
                 return prime_list
         p = count
 
