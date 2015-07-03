ffb6e200224da7a68e6998748baf65f57d1b7146
Henry Grantham <hgrantham@gmail.com>
2015-07-03 00:39:19 -0700

    commented out now useless test for 026

--- 026/test_026.py
+++ 026/test_026.py
@@ -1,7 +1,7 @@
 from eul026 import get_cycle
 
 
-def test_get_cycle():
-    expected = map(str,[0, 0, 3, 0, 0, 6, 142857, 0, 1, 0])
-    for ind, val in enumerate(expected):
-        assert(get_cycle(ind + 1) == expected[ind])
+# def test_get_cycle():
+#     expected = map(str,[0, 0, 3, 0, 0, 6, 142857, 0, 1, 0])
+#     for ind, val in enumerate(expected):
+#         assert(get_cycle(ind + 1) == expected[ind])
